28a36620e279d37ce6a9c5430696a0d44443a6be
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2024-07-09 13:19:08 -0300

    update
    update

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
+from infrasctructure.database import init_db
+
 import sys
-from PyQt6 import QtWidgets
+import argparse
+
+def run_web_app():
+    from web.app import app
+    app.run()
 
 
-class MyInterface(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+def run_desktop_app():
+    from PyQt5.QtWidgets import QApplication
+    from infrasctructure.repositories import SQLitePersonRepository
+    from application.uses_cases import PersonUseCase
+    from desktop.main import MainWindow
 
-        #Criando os Widgets
-        self.label = QtWidgets.QLabel("Digite seu nome:")
-        self.input_nome = QtWidgets.QLineEdit()
-        self.btn_hellow = QtWidgets.QPushButton("Hello")
+    app = QApplication(sys.argv)
 
-        #Definindo o layout
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.input_nome)
-        layout.addWidget(self.btn_hellow)
-        self.setLayout(layout)
+    person_repository = SQLitePersonRepository()
+    person_use_case = PersonUseCase(person_repository)
 
-        #Conectando os sinais aos slots
-        self.btn_hellow.clicked.connect(self.greetings)
-
-    def greetings(self):
-        name = self.input_nome.text()
-        message = f"Hello, {name}"
-        QtWidgets.QMessageBox.information(self, "Saudação", message)
-
-
-if __name__ =="__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = MyInterface()
+    window = MainWindow(person_use_case)
     window.show()
-    sys.exit(app.exec())
+
+    sys.exit(app.exec_())
+
+
+if __name__=='__main__':
+    init_db()
+
+    parser = argparse.ArgumentParser(description='Run the application.')
+    parser.add_argument('--interface', choices=['web', 'desktop'], required=True, help='Choose the interface to run')
+    args = parser.parse_args()
+
+    try:
+        if args.interface == 'web':
+            run_web_app()
+        elif args.interface == 'desktop':
+            run_desktop_app()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
