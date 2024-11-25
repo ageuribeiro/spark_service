fc7f73217b661a7cc8f8b8394c2b39043b6134dc
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2024-11-25 12:58:20 -0300

    Alterando endereço ip da aplicação.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def hello():
     return 'Hello World'
 
 if(__name__) == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port,debug=True)
