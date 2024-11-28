4419bd8725c0aa86046cb51ffa4aef1cf40101c1
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2024-11-28 20:33:11 -0300

    alteração do método HTTP no index

--- app/views/general_views.py
+++ app/views/general_views.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 general_bp = Blueprint('general', __name__)
 
-@general_bp.route('/')
+@general_bp.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
