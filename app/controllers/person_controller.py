257cc364876cad67aaedab877fd1a7053562154c
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2024-11-28 23:35:22 -0300

    Alterações relevantes

--- app/controllers/person_controller.py
+++ app/controllers/person_controller.py
@@ -1,13 +1,15 @@
-from .utils import error_handler, PersonSchema
 from flask import Blueprint, render_template, request, jsonify
-from ..models import Person
+from .utils import error_handler, PersonSchema
 from app import mongo_db
+from bson.objectid import ObjectId
+import logging
+from ..models import Person
 
 person_bp = Blueprint('person_bp', __name__)
 
-@person_bp.route('/person', methods=['GET','POST'], endpoint='handle_persons_api')
+@person_bp.route('/person', methods=['GET'], endpoint='get_persons')
 @error_handler
-def handle_persons():
+def get_persons():
     
     persons_collections = mongo_db.get_collection('persons')
 
@@ -29,6 +31,17 @@ def handle_persons():
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
-@person_bp.route('/persons', methods=['GET','POST'], endpoint='persons_page')
+@person_bp.route('/persons/<person_id>', methods=['GET'], endpoint='get_person')
+@error_handler
+def get_person(person_id):
+    return render_template('persons.html')
+
+
+@person_bp.route('/persons', methods=['POST'], endpoint='create_person')
+@error_handler
+def create_person():
+    return True
+
+@person_bp.route('/persons/page', endpoint='persons_page')
 def persons_page():
     return render_template('persons.html')
